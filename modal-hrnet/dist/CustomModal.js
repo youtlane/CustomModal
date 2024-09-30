b4519f9c8869812bca4493dccfdfbc94723596ec
youtlane <youtlanehanane@gmail.com>
2024-09-30 16:35:52 +0200

    feat: app build

---
+++ modal-hrnet/dist/CustomModal.js
@@ -0,0 +1,29 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _react = _interopRequireDefault(require("react"));
+require("./customModal.css");
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
+// Import styles
+
+const CustomModal = _ref => {
+  let {
+    isOpen,
+    onClose,
+    children
+  } = _ref;
+  if (!isOpen) return null; // If not open, don't render the modal
+
+  return /*#__PURE__*/_react.default.createElement("div", {
+    className: "modal-overlay"
+  }, /*#__PURE__*/_react.default.createElement("div", {
+    className: "modal-content"
+  }, children, /*#__PURE__*/_react.default.createElement("button", {
+    onClick: onClose,
+    className: "modal-close-btn bg-indigo-600 text-white rounded-lg px-4 py-2 text-sm font-medium shadow-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+  }, "Close")));
+};
+var _default = exports.default = CustomModal;
---
+++ modal-hrnet/dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "CustomModal", {
+  enumerable: true,
+  get: function () {
+    return _CustomModal.default;
+  }
+});
+var _CustomModal = _interopRequireDefault(require("./CustomModal.jsx"));
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
